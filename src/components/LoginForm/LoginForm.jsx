f397c6fce11527cf9c1d1f13cf180aaedd188501
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-07-27 17:37:47 +0200

    added form style

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -18,16 +18,21 @@ export const LoginForm = () => {
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className={css.form}>
+      <h2 className={css.title}>User Panel</h2>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label className={css.label}>
+          Email
+          <input type="email" name="email" placeholder="Your Email" />
+        </label>
+        <label className={css.label}>
+          Password
+          <input type="password" name="password" placeholder="Your Password" />
+        </label>
+        <button className={css.button} type="submit">
+          Sign In
+        </button>
+      </form>
+    </div>
   );
 };
